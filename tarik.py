8ab01d183017fdc510faf60de6e4e25e00512b5c
perdanamain <firman.fp123@gmail.com>
2024-12-17 07:50:03 +0000

    feat: add date for fetching

--- tarik.py
+++ tarik.py
@@ -7,14 +7,14 @@ from config import Config
 warnings.filterwarnings("ignore")
 
 # URL yang ingin diakses
-tanggal = "2024-10-03 03:40:30.252"
+tanggal = "2023-03-03 03:40:30.252"
 tanggal = datetime.strptime(tanggal, "%Y-%m-%d %H:%M:%S.%f")
 
 host = Config().PIWEB_API_URL
 
 val = []
 tgl = tanggal.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-url = (f"{host}/streams/F1DPw1kUu10ziUaXEx2rIyo4pAAgwAAAS1RKQi1LSTAwLVBJMVxUSkIzLkNPTkRSIFZBQyBQTVAgQSBTRVAgVEsgTFZM/value")
+url = (f"{host}/streams/F1DPw1kUu10ziUaXEx2rIyo4pAAgwAAAS1RKQi1LSTAwLVBJMVxUSkIzLkNPTkRSIFZBQyBQTVAgQSBTRVAgVEsgTFZM/value?time={tanggal}")
 
 # Username dan Password
 username = Config().PIWEB_API_USER
